7f443a5bca3fb4bc2b4d0ee585bbf1a831297d74
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-10-20 10:45:16 +0200

    Set display property of backToTop div to hidden

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+window.onscroll = function()
+{
+    if (pageYOffset >= 150) {
         document.getElementById("backToTop").style.display = "block";
     } else {
         document.getElementById("backToTop").style.display = "none";
     }
-}
+};
