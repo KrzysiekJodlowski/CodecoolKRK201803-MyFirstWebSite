b33fb44ac1c74d3b597e5cd1fd72399a2f4a3e1d
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-10-20 11:23:20 +0200

    Adjust header content scaling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.onscroll = function()
 {
-    if (pageYOffset >= 150) {
+    if (pageYOffset >= 120) {
         document.getElementById("backToTop").style.display = "block";
     } else {
         document.getElementById("backToTop").style.display = "none";
